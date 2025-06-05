b57883de4543d33b45a0cedd25895e6a13efb31c
DarkStar <pegasus@quantasource.dev>
2025-06-05 17:53:25 -0500

    try

--- src/ansible/dev.sh
+++ src/ansible/dev.sh
@@ -3,4 +3,4 @@ echo "*/" > roles/dev/.gitignore
 
 cd roles/dev
 git clone git@github.com:UniAppStore/ansible-role-Brave-browser.git
-git clone git@github.com:Unicorn-OS/ansible-role-Yay-AUR-packager.git
+git clone git@github.com:Unicorn-OS/ansible-role-Yay-AUR-package-manager.git
